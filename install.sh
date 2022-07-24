65b8466cfc832c2210bed6c92fd53182562b11ce
cqroot <cqroot@outlook.com>
2022-07-24 21:09:52 +0800

    feat: add desktop-entry patch

--- install.sh
+++ install.sh
@@ -4,8 +4,10 @@ ST_DIR=../st/
 
 PATCHES_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+patch --merge -d ${ST_DIR} -i ${PATCHES_DIR}/st-make-baa9357.diff
 patch --merge -d ${ST_DIR} -i ${PATCHES_DIR}/st-right-click-paste-baa9357.diff
 patch --merge -d ${ST_DIR} -i ${PATCHES_DIR}/st-copy-on-select-baa9357.diff
 patch --merge -d ${ST_DIR} -i ${PATCHES_DIR}/st-font-baa9357.diff
 patch --merge -d ${ST_DIR} -i ${PATCHES_DIR}/st-shell-baa9357.diff
-patch --merge -d ${ST_DIR} -i ${PATCHES_DIR}/st-dracula-0.8.5.diff
+patch --merge -d ${ST_DIR} -i ${PATCHES_DIR}/st-dracula-baa9357.diff
+patch --merge -d ${ST_DIR} -i ${PATCHES_DIR}/st-desktop-entry-baa9357.diff
